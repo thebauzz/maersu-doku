23f08d893ad8f766b010af3181fd4231e24ab40c
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-08-09 07:03:11 +0200

    commented js - v180809-07.03

--- html/navigation/nav_right_sticky/nav_right_sticky.js
+++ html/navigation/nav_right_sticky/nav_right_sticky.js
@@ -1,4 +1,5 @@
 window.onscroll = schongescrollt;
+// window.onload = neuerinhalt
 
 function schongescrollt() {
 
@@ -12,3 +13,33 @@ function schongescrollt() {
     dasda.classList.remove("blibdobe")
   }
 }
+/*
+function neuerinhalt() {
+// erstellt neues p Element mit id zeit und mit inhalt Hallo Zäme
+  var element = document.createElement('p');
+  element.textContent = "Hallo Zäme";
+  element.setAttribute('id', 'zeit');
+  document.getElementById("neuerinhalt").appendChild(element);
+
+// erstellt
+  var elementZeit = document.createElement('p');
+  var stundeJetzt = new Date().getHours();
+  var greeting;
+
+  if (stundeJetzt > 18) {
+    greeting = 'Guete Abig!';
+  }
+  else if (stundeJetzt > 12) {
+    greeting = 'Guete Tag!';
+  }
+  else if (stundeJetzt > 0) {
+    greeting = 'Guete Morgen!'
+  }
+  else  {
+    greeting = 'Willkommen!';
+  }
+
+  elementZeit.textContent = greeting;
+  document.getElementById("neuerinhalt").appendChild(elementZeit);
+}
+*/
