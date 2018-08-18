e2e7f6513c87b772a923b5c26b4a3594c72a23d3
marcel <marcel@gertsch.be>
2018-08-18 22:39:18 +0200

    nav done - v180818-22.39

--- html/marcel2/js/main.js
+++ html/marcel2/js/main.js
@@ -13,19 +13,82 @@ function schongescrollt() {
 }
 
 function menu_icon_change() {
+  var einstgear  = document.getElementById("nav-einst-gear");
+  var menulinien = document.getElementById("nav-menu-linien");
+  var menuclose  = document.getElementById("nav-menu-close");
 
-  var menulinien = document.getElementById("menu-linien");
-  var menuclose  = document.getElementById("menu-close");
-
+  einstgear.classList.toggle("unsichtbar");
   menulinien.classList.toggle("menu-weg");
-   menuclose.classList.toggle("menu-weg");
+  menuclose.classList.toggle("menu-weg");
+
+  var nav = document.getElementById("nav-menu-nav");
+  nav.classList.toggle("nav-rein");
+
+  var dropdown   = document.getElementById("nav-dropdown");
+  if (dropdown.classList.contains("dropdown-rein")) {
+    dropdown.classList.remove("dropdown-rein");
+  }
+
+  var empty = document.getElementById("leerecontainer");
+  empty.classList.toggle("unsichtbar");
 }
 
 function einst_icon_change() {
+  var menulinien = document.getElementById("nav-menu-linien");
+  var einstgear  = document.getElementById("nav-einst-gear");
+  var einstclose = document.getElementById("nav-einst-close");
 
-  var einstgear  = document.getElementById("einst-gear");
-  var einstclose = document.getElementById("einst-close");
-
-   einstgear.classList.toggle("einst-weg");
+  menulinien.classList.toggle("unsichtbar");
+  einstgear.classList.toggle("einst-weg");
   einstclose.classList.toggle("einst-weg");
+
+  var einst = document.getElementById("nav-einst-einst");
+  einst.classList.toggle("einst-rein");
+
+  var empty = document.getElementById("leerecontainer");
+  empty.classList.toggle("unsichtbar");
+}
+
+function dropdown_aktiv() {
+  var ddicon = document.getElementById("dropdownicon");
+  var zeigen = document.getElementById("nav-dropdown");
+
+  ddicon.classList.toggle("drehen");
+  zeigen.classList.toggle("dropdown-rein")
+
+}
+
+function wegmitallem() {
+  var nav = document.getElementById("nav-menu-nav");
+
+  if (nav.classList.contains("nav-rein")) {
+    nav.classList.remove("nav-rein");
+
+    var einstgear = document.getElementById("nav-einst-gear");
+    einstgear.classList.toggle("unsichtbar");
+
+    var menulinien = document.getElementById("nav-menu-linien");
+    menulinien.classList.toggle("menu-weg");
+
+    var menuclose = document.getElementById("nav-menu-close");
+    menuclose.classList.toggle("menu-weg");
+  }
+
+  var einst = document.getElementById("nav-einst-einst")
+
+  if (einst.classList.contains("einst-rein")) {
+    einst.classList.remove("einst-rein");
+
+    var navlinie = document.getElementById("nav-menu-linien");
+    navlinie.classList.toggle("unsichtbar");
+
+    var einstgear = document.getElementById("nav-einst-gear");
+    einstgear.classList.toggle("einst-weg");
+
+    var einstclose = document.getElementById("nav-einst-close");
+    einstclose.classList.toggle("einst-weg");
+  }
+
+  var unsichtbar = document.getElementById("leerecontainer");
+  unsichtbar.classList.toggle("unsichtbar")
 }
