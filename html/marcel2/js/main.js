7de6e86fe598e460009db020f30c450738f309d5
marcel <marcel@gertsch.be>
2018-08-18 20:08:32 +0200

    new structure - v180818-20.08

--- html/marcel2/js/main.js
+++ html/marcel2/js/main.js
@@ -5,9 +5,27 @@ function schongescrollt() {
   var sticky = document.getElementById("stickynav");
 
   if (scrollposition >= 300) {
-    sticky.classList.add("fixednav");
+    sticky.classList.add("nav-fixed");
   }
   else {
-    sticky.classList.remove("fixednav");
+    sticky.classList.remove("nav-fixed");
   }
+}
+
+function menu_icon_change() {
+
+  var menulinien = document.getElementById("menu-linien");
+  var menuclose  = document.getElementById("menu-close");
+
+  menulinien.classList.toggle("menu-weg");
+   menuclose.classList.toggle("menu-weg");
+}
+
+function einst_icon_change() {
+
+  var einstgear  = document.getElementById("einst-gear");
+  var einstclose = document.getElementById("einst-close");
+
+   einstgear.classList.toggle("einst-weg");
+  einstclose.classList.toggle("einst-weg");
 }
