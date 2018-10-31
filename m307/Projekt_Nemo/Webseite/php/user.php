24f9790a4dcdd88ae59a5324f06632fcf82e8dd2
Neeemoo <33062961+Neeemoo@users.noreply.github.com>
2018-10-31 09:01:25 +0100

    Revert "progress - v181031-09.00"
    This reverts commit a5d4bf99fcc189102e95bfe698fdba3f584798fa.

--- m307/Projekt_Nemo/Webseite/php/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['username'])){
-	$string = " narcisa admin dbuser Barbara Claudio Peter Fritz Hans Oliver Kurt";
-	$un = htmlspecialchars($_POST['username']);
-	if(stripos($string,$un)==true) echo trim("false");
-		else echo trim("true");
-}
-else die
-?>
