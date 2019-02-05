471ce1c313f8a3ef1136b2cf2b96e06a432f9fd8
marcel <marcel@gertsch.be>
2019-02-05 23:22:14 +0100

    progress - v190205-23.22

--- html/time/time.js
+++ html/time/time.js
@@ -3,17 +3,24 @@ window.onload = currentTime;
 function currentTime() {
   time = new Date();
 
+  dd = time.getDate();
+  mm = time.getMonth() + 1;
+  yy = time.getFullYear();
+
   h = time.getHours();
-  h = checkTime(h);
-
   m = time.getMinutes();
-  m = checkTime(m);
-
   s = time.getSeconds();
+
+  dd = checkTime(dd);
+  mm = checkTime(mm);
+
+  h = checkTime(h);
+  m = checkTime(m);
   s = checkTime(s);
 
+  document.getElementById('date-display').innerHTML = dd + "/" + mm + "/" + yy;
   document.getElementById('time-display').innerHTML = h + ":" + m + ":" + s;
-  var y = setTimeout(currentTime, 10);
+  var y = setTimeout(currentTime, 100);
 }
 
 function checkTime(x) {
