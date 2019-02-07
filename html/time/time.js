974dba6589d90eb476c36c53b8612f6d5ad7cb82
marcel <marcel@gertsch.be>
2019-02-07 18:33:31 +0100

    progress - v190207-18.33

--- html/time/time.js
+++ html/time/time.js
@@ -3,23 +3,28 @@ window.onload = currentTime;
 function currentTime() {
   time = new Date();
 
+  dNamen = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  mNamen = ["jan", "feb", "mar", "Apr", "Jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+  mIndex = time.getMonth();
+  dIndex = time.getDay() - 1;
+
+  ddd= dNamen[dIndex];
   dd = time.getDate();
-  mm = time.getMonth() + 1;
-  yy = time.getFullYear();
+  mm = mNamen[mIndex];
 
   h = time.getHours();
   m = time.getMinutes();
   s = time.getSeconds();
 
   dd = checkTime(dd);
-  mm = checkTime(mm);
 
   h = checkTime(h);
   m = checkTime(m);
   s = checkTime(s);
 
-  document.getElementById('date-display').innerHTML = dd + "/" + mm + "/" + yy;
-  document.getElementById('time-display').innerHTML = h + ":" + m + ":" + s;
+  document.getElementById('date-display').innerHTML = ddd+", "+dd+" "+mm;
+  document.getElementById('time-display').innerHTML = h+":"+m+":"+s;
   var y = setTimeout(currentTime, 100);
 }
 
