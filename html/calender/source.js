c64db2017c58391beb11d92b48930f67a44c58d2
marcel <marcel@gertsch.be>
2019-02-17 08:38:29 +0100

    progress - v190217-08.38

--- html/calender/source.js
+++ html/calender/source.js
@@ -6,6 +6,7 @@ var source = [
   "2019;2;29;Mami;var(--bday)",
   "2019;6;31;me;var(--bday)",
   "2019;7;22;Roman;var(--bday)",
+  "2019;8;3;Monika;var(--bday)",
   "2019;8;23;Paps;var(--bday)",
   "2019;9;20;Emma;var(--bday)",
 
@@ -13,9 +14,15 @@ var source = [
 
   "2019;1;2;3. Teil A-Liga;var(--normal)",
   "2019;1;3;3. Teil A-Liga;var(--normal)",
-  "2019;1;14;Valentine's Day @Lausanne;var(--normal)",
+  "2019;1;14;Valentine's Day Lausanne;var(--normal)",
   "2019;1;16;At Emma's Place;var(--normal)",
   "2019;1;17;At Emma's Place;var(--normal)",
   "2019;2;2;Help out C-SM Bern;var(--normal)",
-  "2019;2;3;Training @Küsnacht;var(--normal)",
+  "2019;2;3;Training Küsnacht;var(--normal)",
+  "2019;2;8;SM A-Liga;var(--normal)",
+  "2019;2;9;SM A-Liga;var(--normal)",
+  "2019;2;10;SM A-Liga;var(--normal)",
+  "2019;2;15;SM A-Liga;var(--normal)",
+  "2019;2;16;SM A-Liga;var(--normal)",
+  "2019;2;17;SM A-Liga;var(--normal)",
 ]
